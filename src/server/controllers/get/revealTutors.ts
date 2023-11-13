cc5b73613effaf4c1ff9983159418658f69527bc
Julio Cesar <juliomtd23@gmail.com>
2023-11-13 16:46:26 -0300

    Atualização final revelar tutores

--- src/server/controllers/get/revealTutors.ts
+++ src/server/controllers/get/revealTutors.ts
@@ -1,12 +1,6 @@
 import { Request, Response } from "express";
-
 import Tutor from "../../database/models/tutors";
 import Pets from "../../database/models/pets";
-/*import clients from "../../database/dados";
-
-export const revealTutors = (req:Request, res:Response) => {
-    res.json(clients)
-}*/
 
 export const revealTutors = async (req:Request, res:Response) => {
     const showUser = await Tutor.find()
